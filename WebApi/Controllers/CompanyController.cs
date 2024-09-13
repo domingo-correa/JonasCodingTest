e5ea030d0c9d09d5d1f19df4ac584c0cf10ceca1
domingo-correa <109610034+domingo-correa@users.noreply.github.com>
2024-09-13 19:25:16 -0400

    Update CompanyController.cs

--- WebApi/Controllers/CompanyController.cs
+++ WebApi/Controllers/CompanyController.cs
@@ -4,6 +4,9 @@ using System.Web.Http;
 using AutoMapper;
 using BusinessLayer.Model.Interfaces;
 using WebApi.Models;
+using Prog12_Logging.Core;  
+using Prog12_Logging.IRepository;  
+using Prog12_Logging.Model;
 
 namespace WebApi.Controllers
 {
@@ -11,51 +14,98 @@ namespace WebApi.Controllers
     {
         private readonly ICompanyService _companyService;
         private readonly IMapper _mapper;
+        private readonly ILogger _logger;  
 
-        public CompanyController(ICompanyService companyService, IMapper mapper)
+        public CompanyController(ICompanyService companyService, IMapper mapper, ILogger<TodoController> logger)
         {
             _companyService = companyService;
             _mapper = mapper;
+            _logger = logger;
         }
         
         // GET api/<controller>
         [HttpGet]
         public IEnumerable<CompanyDto> GetAll()
         {
-            var items = _companyService.GetAllCompanies();
-            return _mapper.Map<IEnumerable<CompanyDto>>(items);
+            try
+                {
+                    var items = _companyService.GetAllCompanies();
+                    return _mapper.Map<IEnumerable<CompanyDto>>(items);
+                }
+            catch (Exception ex)
+            {
+                var msg = "An error occurred while listing all companies. " + DateTime.Now;
+                _logger.LogError(ex, msg);
+                throw new Exception(msg);
+            }
         }
 
         // GET api/<controller>/5
         [HttpGet("{companyCode:string}")]
         public CompanyDto Get(string companyCode)
         {
-            var item = _companyService.GetCompanyByCode(companyCode);
-            return _mapper.Map<CompanyDto>(item);
+            try
+                {
+                    var item = _companyService.GetCompanyByCode(companyCode);
+                    return _mapper.Map<CompanyDto>(item);                
+                }
+            catch (Exception ex)
+            {
+                var msg = "An error occurred while retrieving " + companyCode + " company. " + DateTime.Now;
+                _logger.LogError(ex, msg);
+                throw new Exception(msg);
+            }    
         }
 
         // POST api/<controller>
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] Company company)
         {
-            await _companyService.AddCompany(company);
-            return Ok(new { message = "Company successfully created" });
+            try
+                {
+                    await _companyService.AddCompany(company);
+                    return Ok(new { message = "Company successfully created" });
+                }
+            catch (Exception ex)
+            {
+                var msg = "An error occurred while creting a company. " + DateTime.Now;
+                _logger.LogError(ex, msg);
+                throw new Exception(msg);
+            }    
         }
 
         // PUT api/<controller>/5
         [HttpPut("{id:int}")]
         public async Task<IActionResult> Put(int id, [FromBody] Company company)
         {
-            await _companyService.AddCompany(company);
-            return Ok(new { message = "Company successfully updated" });
+            try
+                {
+                    await _companyService.AddCompany(company);
+                    return Ok(new { message = "Company successfully updated" });
+                }
+            catch (Exception ex)
+            {
+                var msg = "An error occurred while updating company id: " + id + ". " + DateTime.Now;
+                _logger.LogError(ex, msg);
+                throw new Exception(msg);
+            }    
         }
 
         // DELETE api/<controller>/5
         [HttpDelete("{id:int}")]
         public async Task<IActionResult> Delete(int id)
         {
-            await _companyService.DeleteCompany(id);
-            return Ok(new { message = $"Company successfully deleted" });
+            try
+                {
+                    await _companyService.DeleteCompany(id);
+                    return Ok(new { message = $"Company successfully deleted" });
+                }
+            catch (Exception ex)
+            {
+                vvar msg = "An error occurred while deleting company id: " + id + ". " + DateTime.Now;
+                _logger.LogError(ex, msg);
+                throw new Exception(msg);
+            }    
         }
     }
 }
