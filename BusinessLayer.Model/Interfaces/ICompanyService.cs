d8a54d907f99b9571a0a38e745a3df853426d2b0
domingo-correa <109610034+domingo-correa@users.noreply.github.com>
2024-09-13 15:53:59 -0400

    Update ICompanyService.cs

--- BusinessLayer.Model/Interfaces/ICompanyService.cs
+++ BusinessLayer.Model/Interfaces/ICompanyService.cs
@@ -7,8 +7,8 @@ namespace BusinessLayer.Model.Interfaces
     {
         Task<IEnumerable<CompanyInfo>> GetAllCompanies();
         Task<CompanyInfo?> GetCompanyByCode(string companyCode);
-        Task<CompanyInfo> AddCompany(AddCompany request);
-        Task<CompanyInfo> UpdateCompany(int id, UpdateCompany request);
+        Task<CompanyInfo> AddCompany(CompanyInfo addCompany);
+        Task<CompanyInfo> UpdateCompany(int id, CompanyInfo updateCompany);
         Task<bool> DeleteCompany(int id);
     }
 }
