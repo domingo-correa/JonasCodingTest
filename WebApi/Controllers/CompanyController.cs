fbe8df197450c8e802819dbd354f258724d9a6f9
domingo-correa <109610034+domingo-correa@users.noreply.github.com>
2024-09-13 14:49:29 -0400

    Update CompanyController.cs

--- WebApi/Controllers/CompanyController.cs
+++ WebApi/Controllers/CompanyController.cs
@@ -17,14 +17,17 @@ namespace WebApi.Controllers
             _companyService = companyService;
             _mapper = mapper;
         }
+        
         // GET api/<controller>
-        public IEnumerable<CompanyDto> GetAll()
+        [HttpGet]
+        public async Task<IEnumerable<CompanyDto>> GetAll()
         {
             var items = _companyService.GetAllCompanies();
             return _mapper.Map<IEnumerable<CompanyDto>>(items);
         }
 
         // GET api/<controller>/5
+        [HttpGet("{companyCode:string}")]
         public CompanyDto Get(string companyCode)
         {
             var item = _companyService.GetCompanyByCode(companyCode);
@@ -32,18 +35,45 @@ namespace WebApi.Controllers
         }
 
         // POST api/<controller>
-        public void Post([FromBody]string value)
+        [HttpPost]
+        public void Post([FromBody] Company company)
         {
+            if (employee == null)
+                return BadRequest();
+
+            var createdCompany = await _companyService.AddCompany(company);
+
+            return CreatedAtAction(nameof(Get),
+                new { companyCode = createdCompany.companyCode }, createdCompany);
         }
 
         // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
+        [HttpPut"{id:int}"]
+        public void Put(int id, [FromBody] Company company)
         {
+            if (id != company.companyCode)
+                return BadRequest("Company code not found");
+
+            var companyToUpdate = await _companyService.GetCompany(id);
+
+            if (companyToUpdate == null)
+                return NotFound($"Company code = {id} not found");
+
+            return await _companyService.UpdateCompany(company);
         }
 
         // DELETE api/<controller>/5
+        [HttpDelete("{id:int}")]
         public void Delete(int id)
         {
+            var companyToDelete = await _companyService.GetCompany(id);
+
+            if (companyToDelete == null)
+            {
+                return NotFound($"Company with Id = {id} not found");
+            }
+
+            return await _companyService.DeleteCompany(id);
         }
     }
 }
