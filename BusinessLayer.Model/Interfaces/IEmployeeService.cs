7dd353dc9190cd6da1f7225a28fedf1c570b77cd
domingo-correa <109610034+domingo-correa@users.noreply.github.com>
2024-09-13 20:40:20 -0400

    Create IEmployeeService.cs

---
+++ BusinessLayer.Model/Interfaces/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using BusinessLayer.Model.Models;
+
+namespace BusinessLayer.Model.Interfaces
+{
+    public interface IEmployeeService
+    {
+        Task<IEnumerable<EmployeeInfo>> GetAllEmployees();
+        Task<EmployeeInfo?> GetEmployeeById(string employeeId);
+        Task AddEmployee(EmployeeInfo addEmployee);
+        Task UpdateCEmployee(EmployeeInfo updateEmployee);
+        Task DeleteEmployee(int id);
+    }
+}
