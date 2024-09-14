dc777fcf57115232dc89c097f79dd8bdbbf0f3c9
domingo-correa <109610034+domingo-correa@users.noreply.github.com>
2024-09-13 21:05:54 -0400

    Create IEmployeeRepository.cs

---
+++ DataLayer.Model/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Model.Models;
+
+namespace DataAccessLayer.Model.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetAll();
+        Task<Employee> GetByCode(string employeeCode);
+        Task<bool> SaveEmployee(Employee employee);
+        Task DeleteEmployee(int id)
+    }
+}
